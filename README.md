557e3674d79056524ddac0b0be60958cfc17373a
Emanuel <kraemerseele@gmx.de>
2014-02-04 22:12:51 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+NeverGonnaResize.js
+=====
+
+This is a small `AudioContext`-experiment, playing tasteful music while a visitor is resizing the browser-window, hunting down your media-queries. Unfortunately it only works in Chrome, though the `AudioContext` has actually a wider support.
+
+### [Visit the demo …](http://herschel666.github.io/NeverGonnaResize.js)
+
+… and resize the hell out of your browser-window to see what I'm talking about.
+
+## Usage
+
+Put the script-file into your website and initialize **NeverGonnaResize.js**:
+
+	<script src="./path/to/never-gonna-resize.js"></script>
+	<script>
+		neverGonnaResize('../path/to/tasteful-music.mp3');
+	</script>
+
+That's it!
+
+## Meta
+
+If you have questions, open an issue or contact me via [twitter](https://twitter.com/Herschel_R).
+
+## License
+
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 2, December 2004
+
+Copyright (C) 2014 Emanuel Kluge
+
+Everyone is permitted to copy and distribute verbatim or modified of this license document, and changing it is allowed as long the name is changed.
+
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+0. You just DO WHAT THE FUCK YOU WANT TO.
---
+++ src/never-gonna-resize.js
@@ -0,0 +1,103 @@
+/**
+ * NeverGonnaResize.js - v0.0.1
+ *
+ * Emanuel Kluge - 2014-02-04
+ *
+ * https://github.com/herschel666/NeverGonnaResize.js
+ *
+ * License: WTFPL
+ */
+(function (win, AudioCtx, undefined) {
+
+  var MAX_WIDTH = win.screen.availWidth;
+  var MIN_WIDTH = 320;
+
+  var timeoutId;
+
+  var audioCtx,
+      source,
+      filter,
+      gainNode;
+
+  function onResponse(evnt) {
+    audioCtx.decodeAudioData(evnt.target.response, function decode(buffer) {
+
+      source = audioCtx.createBufferSource();
+      filter = audioCtx.createBiquadFilter();
+      gainNode = audioCtx.createGain();
+
+      source.buffer = buffer;
+      gainNode.gain.value = 0;
+      filter.gain.value = 15;
+      filter.Q.value = 10;
+
+      source.connect(filter);
+      filter.connect(gainNode);
+      gainNode.connect(audioCtx.destination);
+
+      source.start();
+
+      win.addEventListener('resize', onResize);
+
+    });
+  }
+
+  function onResize() {
+
+    filter.frequency.value = Math.floor(getFrequency(win.innerWidth));
+    gainNode.gain.value = .4;
+
+    if ( timeoutId ) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(afterResize, 1000);
+
+  }
+
+  function afterResize() {
+    gainNode.gain.value = 0;
+  }
+
+  function getFrequency(winWidth) {
+
+    var factor = (winWidth - MIN_WIDTH) / (MAX_WIDTH - MIN_WIDTH),
+        range = 5000 - filter.frequency.minValue,
+        result;
+
+    if ( factor < 0 ) {
+      factor = 0;
+    }
+
+    if ( factor > 1 ) {
+      factor = 1;
+    }
+
+    result = range - factor * range;
+
+    return result < filter.frequency.minValue
+      ? filter.frequency.minValue
+      : result > 5000
+      ? 5000
+      : result;
+
+  }
+
+  win.neverGonnaResize = function neverGonnyResize(pathToHodor) {
+
+    var request;
+
+    if ( !AudioCtx ) {
+      return;
+    }
+
+    audioCtx = new AudioCtx();
+
+    request = new XMLHttpRequest();
+    request.open('GET', pathToHodor, true);
+    request.responseType = 'arraybuffer';
+    request.addEventListener('load', onResponse, false);
+    request.send();
+
+  };
+
+})(window, (window.AudioContext || window.webkitAudioContext || window.oAudioContext || window.mozAudioContext || window.msAudioContext));
